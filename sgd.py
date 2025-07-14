c0dfb4843dc9aa8d9f2f13a9ca59845c5abaf9ac
0xC4LL3 <76694291+0xC4LL3@users.noreply.github.com>
2025-07-14 12:59:03 +0200

    implemented PSR variant with batched estimator calls for efficiency

--- sgd.py
+++ sgd.py
@@ -43,7 +43,7 @@ class SGD:
         return params, cost
 
     def step(self, params):
-        grad = self.parameter_shift_grad(params)
+        grad = self.parameter_shift_grad_v2(params)
         return params - self.learning_rate * grad
 
     def parameter_shift_grad(self, params):
@@ -58,6 +58,27 @@ class SGD:
             minus = self.cost_function(shifted)
             
             grad[i] = 0.5 * (plus - minus)
+        return grad
+    
+    def parameter_shift_grad_v2(self, params):
+        n = len(params)
+        pubs = []
+        # build batch of plus/minus shifted parameters
+        for i in range(n):
+            plus = params.copy()
+            plus[i] += np.pi/2
+            pubs.append((self.ansatz, [self.hamiltonian], [plus]))
+            minus = params.copy()
+            minus[i] -= np.pi/2
+            pubs.append((self.ansatz, [self.hamiltonian], [minus]))
+        # run one batched call
+        results = self.estimator.run(pubs=pubs).result()
+        # extract energies and form gradient
+        grad = np.zeros(n)
+        for i in range(n):
+            e_plus = results[2*i].data.evs[0]
+            e_minus = results[2*i+1].data.evs[0]
+            grad[i] = 0.5 * (e_plus - e_minus)
         return grad
     
     def cost_function(self, params):
